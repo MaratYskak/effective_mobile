7c492c7ad75d6d83ff2c1f3129cf10f0bfd73fba
MaratYskak <yskakmarat2@gmail.com>
2025-03-13 20:11:02 +0500

    update character_entity

--- lib/features/character/domain/entities/character_entity.dart
+++ lib/features/character/domain/entities/character_entity.dart
@@ -3,11 +3,13 @@ class CharacterEntity {
   final String name;
   final String status;
   final String species;
+  final String image;
 
   const CharacterEntity({
     required this.id,
     required this.name,
     required this.status,
     required this.species,
+    required this.image,
   });
 }
