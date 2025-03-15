f70aa3e1c262208430fe629cd64c183465b66c4a
MaratYskak <yskakmarat2@gmail.com>
2025-03-16 03:27:37 +0500

    update remote_repo_impl

--- lib/features/character/data/remote/character_remote_data_source_impl.dart
+++ lib/features/character/data/remote/character_remote_data_source_impl.dart
@@ -5,11 +5,15 @@ import 'package:http/http.dart' as http;
 
 class CharacterRemoteDataSourceImpl implements CharacterRemoteDataSource {
   @override
-  Stream<List<CharacterModel>> getCharacters() async* {
+  Future<List<CharacterModel>> getCharacters() async {
     final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
     if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      yield data.map((json) => CharacterModel.fromJson(json)).toList();
+      // final List<dynamic> data = json.decode(response.body);
+      // yield data.map((json) => CharacterModel.fromJson(json)).toList();
+
+      final Map<String, dynamic> decodedResponse = json.decode(response.body);
+      final List<dynamic> results = decodedResponse['results'];
+      return results.map((json) => CharacterModel.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load characters');
     }
