dd708f50b58e8e3c7c3c6b11c46d4aa45d29556a
MaratYskak <yskakmarat2@gmail.com>
2025-03-16 03:26:21 +0500

    update repo_impl

---
+++ lib/features/character/data/repository/character_repository_impl.dart
@@ -0,0 +1,21 @@
+import 'package:effective_mobile/features/character/data/local/character_local_data_source.dart';
+import 'package:effective_mobile/features/character/data/models/character_model.dart';
+import 'package:effective_mobile/features/character/data/remote/character_remote_data_source.dart';
+import 'package:effective_mobile/features/character/domain/entities/character_entity.dart';
+import 'package:effective_mobile/features/character/domain/repository/character_repository.dart';
+
+class CharacterRepositoryImpl implements CharacterRepository {
+  final CharacterRemoteDataSource remoteDataSource;
+  final CharacterLocalDataSource localDataSource;
+
+  CharacterRepositoryImpl({required this.remoteDataSource, required this.localDataSource});
+
+  @override
+  Future<List<CharacterEntity>> getCharactersFromLocalDB() => localDataSource.getCharacters();
+
+  @override
+  Future<List<CharacterEntity>> getCharactersFromServer() => remoteDataSource.getCharacters();
+
+  @override
+  updateDB(List<CharacterEntity> list) => localDataSource.updateDB(list);
+}
