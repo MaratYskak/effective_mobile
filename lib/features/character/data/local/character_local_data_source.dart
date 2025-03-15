3f412b1b01ca84b87fc0b1b6fc39feacfe88bd81
MaratYskak <yskakmarat2@gmail.com>
2025-03-16 03:28:24 +0500

    update local_repo

--- lib/features/character/data/local/character_local_data_source.dart
+++ lib/features/character/data/local/character_local_data_source.dart
@@ -1,5 +1,7 @@
 import 'package:effective_mobile/features/character/data/models/character_model.dart';
+import 'package:effective_mobile/features/character/domain/entities/character_entity.dart';
 
 abstract class CharacterLocalDataSource {
-  Stream<List<CharacterModel>> getCharacters();
+  Future<List<CharacterModel>> getCharacters();
+  Future<void> updateDB(List<CharacterEntity> characters);
 }
